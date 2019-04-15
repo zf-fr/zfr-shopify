fba237cbac6637592f2f5bab2742fe90eba867cc
Michaël Gallego <mic.gallego@gmail.com>
2019-04-15 14:09:21 +0200

    Fix tests

--- test/ShopifyClientTest.php
+++ test/ShopifyClientTest.php
@@ -63,6 +63,7 @@ class ShopifyClientTest extends TestCase
             [
                 [
                     'shop'        => 'test.myshopify.com',
+                    'version'     => '2019-04',
                     'api_key'     => 'key_123',
                     'private_app' => true,
                     'password'    => 'pass_123'
@@ -72,6 +73,7 @@ class ShopifyClientTest extends TestCase
             [
                 [
                     'shop'         => 'test.myshopify.com',
+                    'version'      => '2019-04',
                     'api_key'      => 'key_123',
                     'private_app'  => false,
                     'access_token' => 'token_123'
@@ -265,6 +267,7 @@ class ShopifyClientTest extends TestCase
     {
         $options = [
             'shop'         => 'test.myshopify.com',
+            'version'      => '2019-04',
             'private_app'  => false,
             'access_token' => 'token_123',
             'api_key'      => 'key'
@@ -277,6 +280,7 @@ class ShopifyClientTest extends TestCase
     {
         $options = [
             'shop'         => 'test.myshopify.com',
+            'version'      => '2019-04',
             'private_app'  => true,
             'password'     => 'password_123',
             'api_key'      => 'key'
